20eb4c49ca1fe1ea107392cc86cc27c376ddf888
Dan Seminara <japaneseforgaming@gmail.com>
2022-01-06 15:06:46 -0500

    Done!

---
+++ db/migrate/001_create_costumes.rb
@@ -0,0 +1,15 @@
+
+
+class CreateCostumes < ActiveRecord::Migration[6.1]
+
+    def change
+        create_table :Costumes do |t|
+            t.string :name
+            t.float :price
+            t.string :size
+            t.string :image_url
+            t.timestamps
+        end
+    end
+
+end
